73fd78068f7f8f93f6ebcf3f3e110f2540dd1806
HaoyuanFeng <hyx466254601@gmail.com>
2020-11-13 13:15:32 +0800

    gengg

--- src/service/confing.js
+++ src/service/confing.js
@@ -1,5 +1,5 @@
 
-const devBaseUrl = 'http://123.207.32.32:9001';
+
 const proBaseUrl = 'https://httpbin.org';
 
 export const BASE_URL = process.env.NODE_ENV === 'development' ? devBaseUrl : proBaseUrl;
